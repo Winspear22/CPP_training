26dc3836399937bd4a9bd8a71a5c3d12d957b9fe
Adnen ALOUI <adaloui@e3r4p11.clusters.42paris.fr>
2022-04-23 15:04:30 +0200

    CPP00 ex01 preparation

---
+++ CPP00/ex01/PhoneBook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaloui <adaloui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 13:09:36 by adaloui           #+#    #+#             */
+/*   Updated: 2022/04/23 13:12:23 by adaloui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iomanip>
+# include <iostream>
+
+class PhoneBook
+{
+	
+};
+
+class Contact
+{
+	
+};
+
+#endif
