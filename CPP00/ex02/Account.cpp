6bafe83587c425f17ff290e29a2f90c6171a2c28
Adnen ALOUI <adaloui@e3r3p9.clusters.42paris.fr>
2022-04-27 16:48:33 +0200

    CPP00 ex02 -- Debut avec initialisation des fonctions

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaloui <adaloui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 16:36:27 by adaloui           #+#    #+#             */
+/*   Updated: 2022/04/27 16:46:00 by adaloui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit )
+{
+    return ;
+}
+
+Account::~Account( void )
+{
+    return ;
+}
+
+static void Account::_displayTimestamp( void )
+{
+    return ;
+}
+
+void Account::makeDeposit( int deposit )
+{
+    return ;
+}
+
+bool Account::makeWithdrawal( int withdrawal )
+{
+    return (true);
+}
+
+int Account::checkAmount( void ) const
+{
+    return ();
+}
+
+void Account::displayStatus( void ) const
+{
+    return ;
+}
