e39758c70f2558e6ad664a82e64b8a208f64c87f
Adnen ALOUI <adaloui@e3r3p11.clusters.42paris.fr>
2022-04-29 20:45:17 +0200

    CPP01 ex03 - Initialisation. Ne compile pas

---
+++ CPP01/ex03/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaloui <adaloui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/29 20:12:10 by adaloui           #+#    #+#             */
+/*   Updated: 2022/04/29 20:18:15 by adaloui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+int main(void)
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
