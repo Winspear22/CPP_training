e881acfc6b3dad4b9409c77bb1bb1a57bcf9a5d3
adaloui <adnen.aloui2@hotmail.com>
2022-05-01 16:16:55 +0200

    CPP01 ex04 -- DEBUT

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -3,19 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adaloui <adaloui@student.42.fr>            +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 20:02:39 by adaloui           #+#    #+#             */
-/*   Updated: 2022/04/30 20:12:27 by adaloui          ###   ########.fr       */
+/*   Updated: 2022/05/01 15:57:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iomanip>
-#include <iostream>
+#include "sed.hpp"
 
 int main(int argc, char **argv)
 {
     (void)argc;
-    (void)argv;
+   // (void)argv;
+	std::string name;
+	name = argv[1];
+	name += ".replace";
+	std::ofstream ofs("salut");
+	
+	ofs << "coucou les amis" << std::endl;
+	ofs.close();
+	
+    /*if (argc > 4 || argc < 4)
+    {
+		std::cout << "\033[1;31mError. Wrong number of arguments.\033[0m" << std::endl;
+		return (1);
+	}*/
     return (0);
 }
---
+++ CPP01/ex04/sed.hpp
@@ -0,0 +1,14 @@
+#ifndef SED_HPP
+# define SED_HPP
+
+# include <iomanip>
+# include <iostream>
+# include <fstream>
+
+class sed
+{
+	sed(void);
+	~sed(void);
+};
+
+#endif
