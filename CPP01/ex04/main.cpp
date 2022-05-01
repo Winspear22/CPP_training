cbdee024239e1aaac211bb3aa8d76e2d275f0a56
adaloui <adnen.aloui2@hotmail.com>
2022-05-01 18:07:39 +0200

    CPP01 ex04 -- Added the .replace feature

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 20:02:39 by adaloui           #+#    #+#             */
-/*   Updated: 2022/05/01 17:37:45 by user42           ###   ########.fr       */
+/*   Updated: 2022/05/01 18:07:32 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,28 +41,12 @@ char *ft_strjoin(const char *s1, const char *s2)
 int main(int argc, char **argv)
 {
     (void)argc;
-   // (void)argv;
-	/*std::string name;
-	std::string replace;
-	name = argv[1];
-	replace = ".replace";
-	std::transform(name.begin(), name.end(), name.begin(), ::tolower);
-	name += ".replace";
-	std::cout << name;*/
 	char *name;
 	
 	name = ft_strjoin(argv[1], ".replace");
-	std::ofstream ofs(name);
-//ofs.open(name);
-	
+	std::ofstream ofs(name);	
 	ofs << "coucou les amis" << std::endl;
 	ofs.close();
-	
-//    if (argc > 4 || argc < 4)
-  //  {
-	//	std::cout << "\033[1;31mError. Wrong number of arguments.\033[0m" << std::endl;
-//		return (1);
-//	}
 	delete [] name;	
     return (0);
 }
