2e9f5174019ffa090d96fe56169ef10bf649c1d2
adaloui <adnen.aloui2@hotmail.com>
2022-05-13 05:00:47 +0200

    CPP02 ex02 -- .hpp en cours avec tous les opérateurs et les nouvelles fonctions

---
+++ CPP02/ex02/Fixed.cpp
@@ -0,0 +1,90 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( void )
+{
+	this->_i = 0;
+	std::cout << "Default constructor called" << std::endl;
+    return ;
+}
+
+Fixed::Fixed( const int i )
+{
+			
+	std::cout << "Int constructor called" << std::endl;
+		this->_i = i << this->_bit;
+	return;
+}
+
+Fixed::Fixed( const float j )
+{
+	int power = ft_pow(2, this->_bit);
+
+			
+	std::cout << "Float constructor called" << std::endl;
+	this->_i = roundf(j * power);
+	return ;
+}
+
+Fixed::Fixed( const Fixed & n )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = n;
+	return ;
+}
+
+Fixed::~Fixed( void )
+{
+	std::cout << "Destructor called" << std::endl;
+    return ;
+}
+
+Fixed & Fixed::operator=( Fixed const & rhs )
+{
+	std::cout << "Copy assignement operator called" << std::endl;
+	if ( this != &rhs )
+		this->_i = rhs._i;
+	return (*this);
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_i);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	this->_i = raw;
+	return ;
+}
+
+int Fixed::toInt( void ) const
+{
+	return (this->_i >> this->_bit);
+}
+
+int		ft_pow(int val, int pow)
+{
+	int		i = 0;
+	int		result;
+	result = 1;
+	while (i < pow)
+	{
+		result *= val;
+		i++;
+	}
+	return (result);
+}
+
+float Fixed::toFloat( void ) const
+{
+	int power = ft_pow(2, this->_bit);
+	float	result = (float)this->_i / power;
+	return (result);}
+
+
+std::ostream & operator<<( std::ostream & o, Fixed const & rhs )
+{
+	o << rhs.toFloat();
+	return (o);
+}
---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,48 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iomanip>
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+public:
+
+    Fixed( void );
+	Fixed( const int i );
+	Fixed( const float j );
+	Fixed( const Fixed & n );
+    ~Fixed( void );
+	Fixed & operator=( Fixed const & rhs );
+	
+	bool operator>( Fixed const & rhs ) const;
+	bool operator<( Fixed const & rhs ) const;
+	bool operator>=( Fixed const & rhs ) const;
+	bool operator<=( Fixed const & rhs ) const;
+	bool operator!=( Fixed const & rhs ) const;
+	bool operator==( Fixed const & rhs ) const;
+	
+	Fixed operator+( Fixed const & rhs ) const;
+	Fixed operator-( Fixed const & rhs ) const;
+	Fixed operator*( Fixed const & rhs ) const;
+	Fixed operator/( Fixed const & rhs ) const;
+
+	Fixed & operator++( void );
+	Fixed & operator--( void );
+	Fixed operator++( int );
+	Fixed operator--( int );
+
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+private:
+	int _i;
+	static const int _bit = 8;
+};
+
+std::ostream & operator<<( std::ostream & o, Fixed const & rhs );
+int				ft_pow(int val, int pow);
+
+#endif
