7f7e74626a333016f69770087630d1290026b2ac
adaloui <adnen.aloui2@hotmail.com>
2022-10-06 05:45:29 +0200

    CPP07 -- ex01

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -5,14 +5,6 @@ void display_tab( const int i )
     std::cout << i << " ";
 }
 
-void display_tab2( const float i )
-{
-    std::cout << std::setw(1) << std::setprecision(4) << i << "f ";
-}
-
-
-#include <unistd.h>
-
 void display_str( std::string str )
 {
     std::cout << str; 
@@ -26,7 +18,6 @@ void    ft_putchar(const char c)
 int main( void )
 {
     int tab[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	float tab_2[10] = {0.71, 1.41, 2.01, 3.14, 4.62, 5.54, 6.05, 7.66, 8.87, 9.07};
     std::string str[6];
     char test_2[32] = "Coucou les amis comment ca va ?";
 
@@ -45,7 +36,7 @@ int main( void )
 	std::cout << "\033[1;31m======================================\033[0m" << std::endl;
     std::cout << "\033[1;36mIMPLICIT USE TEMPLATE WITH STD::STRING\033[0m" << std::endl;
     std::cout << "\033[1;31m======================================\033[0m" << std::endl;
-    iter<std::string>(str, 6, &display_str);
+    iter(str, 6, &display_str);
     std::cout << std::endl;
     std::cout << "\033[1;31m==================================\033[0m" << std::endl;
     std::cout << "\033[1;33mEXPLICIT USE TEMPLATE WITH INTEGER\033[0m" << std::endl;
@@ -66,16 +57,6 @@ int main( void )
     std::cout << "\033[1;36mIMPLICIT USE TEMPLATE WITH CHARS\033[0m" << std::endl;
     std::cout << "\033[1;31m================================\033[0m" << std::endl;
     iter(test_2, 32, &ft_putchar);
-    std::cout << std::endl;
-	std::cout << "\033[1;31m================================\033[0m" << std::endl;
-    std::cout << "\033[1;33mEXPLICIT USE TEMPLATE WITH FLOAT\033[0m" << std::endl;
-    std::cout << "\033[1;31m================================\033[0m" << std::endl;
-    iter<float>(tab_2, 10, &display_tab2);
-    std::cout << std::endl;
-    std::cout << "\033[1;31m================================\033[0m" << std::endl;
-    std::cout << "\033[1;36mIMPLICIT USE TEMPLATE WITH FLOAT\033[0m" << std::endl;
-    std::cout << "\033[1;31m================================\033[0m" << std::endl;
-    iter(tab_2, 10, &display_tab2);
     std::cout << std::endl;
     return (0);
 }
